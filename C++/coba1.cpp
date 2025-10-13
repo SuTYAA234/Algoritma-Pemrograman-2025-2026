337a8812e3fbf17e68b1ff9f905a15489781c8cc
Muhammad_Rivan_Surya <rvn234@gmail.com>
2025-10-13 09:41:07 +0700

    test1

---
+++ C++/coba1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <conio.h> 
+ 
+main() 
+{ 
+    int a = 7; 
+    int b = 3; 
+    char c[25] = "Muhammad Rivan Surya"; 
+ 
+     
+    puts("Nama "); 
+    puts(c); 
+    printf("Terdiri dari %d Hurup dan %d Kata ", a, b); 
+}
---
+++ JAVA/coba1.java
@@ -0,0 +1,13 @@
+class coba1 { 
+    public static void main(String[] args) { 
+        String nim = "I.2510393"; 
+        String nama = "Muhammad Rivan Surya"; 
+        String umur = "21"; 
+        String profesi = "mahasiswa"; 
+ 
+        System.out.println(nim); 
+        System.out.println(nama); 
+        System.out.println(umur); 
+        System.out.println(profesi); 
+    } 
+} 
