36008c6aa6e38012af4876378f97c57ec88c4fe7
Muhammad_Rivan_Surya <rvn234@gmail.com>
2025-10-13 11:01:33 +0700

    test2

---
+++ PHP/tipe_data.php
@@ -0,0 +1,12 @@
+<?php
+
+$nama = "Muhammad Rivan Surya";
+$umur = 21;
+$tinggi = 170;
+$aktif = true;
+
+echo "Nama: $nama<br>";
+echo "Umur: $umur<br>";
+echo "Tinggi: $tinggi<br>";
+echo "Status aktif: $aktif<br>";
+?>
---
+++ PYTHON/tipe_data.py
@@ -0,0 +1,9 @@
+nama = "Muhammad Rivan Surya"
+umur = 21
+tinggi = 170
+aktif = True
+
+print("Nama:", nama)
+print("Umur:", umur)
+print("Tinggi:", tinggi)
+print("Status aktif:", aktif)
