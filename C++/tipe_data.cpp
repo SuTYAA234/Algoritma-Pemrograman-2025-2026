57ddf75cd2e25624ec5f54b98167a588dce42918
Muhammad_Rivan_Surya <rvn234@gmail.com>
2025-10-13 10:45:23 +0700

    test2

---
+++ C++/tipe_data.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+string nama = "Muhammad Rivan Surya";
+int umur = 21;
+float tinggi = 170;
+bool aktif = true;
+
+    cout << "Nama: " << nama << endl;
+    cout << "Umur: " << umur << endl;
+    cout << "Tinggi: " << tinggi << endl;
+    cout << "Status aktif: " << aktif<< endl;
+
+    system("pause");
+    return 0;
+    
+}
---
+++ JAVA/tipe_data.java
@@ -0,0 +1,14 @@
+public class tipe_data {
+    public static void main(String[] args) {
+        String nama = "Muhammad Rivan Surya";
+        int umur = 21;
+        double tinggi = 170;
+        boolean aktif = true;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Status aktif: " + aktif);
+    }
+
+}
