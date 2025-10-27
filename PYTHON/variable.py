bc838c3c83f205dad70d52b7df2fc933d220fa7c
Muhammad_Rivan_Surya <rvn234@gmail.com>
2025-10-27 08:45:59 +0700

    latihan py

---
+++ PYTHON/variable.py
@@ -0,0 +1,11 @@
+print('---Belajar Variabel---') 
+char = 'A' 
+string = "Muhammad Rivan Surya" 
+a = 21 
+penghasilan1 = 1000000 
+nilai_uts = 82.25 
+print(char) 
+print(string) 
+print(a) 
+print(penghasilan1) 
+print(nilai_uts)
